14f7078084f49dc85ee811a7f37ac7cb3fdfa4bf
shivangjhalani <shivang2004jhalani@gmail.com>
2023-12-04 19:16:00 +0530

    Tried-theming-failed...

--- src/components/margin-width-wrapper.tsx
+++ src/components/margin-width-wrapper.tsx
@@ -1,13 +1,5 @@
-import { ReactNode } from 'react';
+import { ReactNode } from "react";
 
-export default function MarginWidthWrapper({
-  children,
-}: {
-  children: ReactNode;
-}) {
-  return (
-    <div className="flex flex-col md:ml-60 sm:border-r sm:border-zinc-700 min-h-screen">
-      {children}
-    </div>
-  );
+export default function MarginWidthWrapper({ children }: { children: ReactNode }) {
+  return <div className="flex flex-col md:ml-60 sm:border-r sm:border-zinc-700 min-h-screen">{children}</div>;
 }
